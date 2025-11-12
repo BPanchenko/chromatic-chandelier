df10f6b7ff4eed5cedba5e7c70683d6e5199d82d
Boris Vasilievich Panchenko <oberon@list.ru>
2025-11-12 03:09:15 +0300

    update<correlator>: improving interpolation algorithms based on vector calculus

--- correlator.d.ts
+++ correlator.d.ts
@@ -1,133 +1,46 @@
-export type BezierPoints = [p1x: number, p1y: number, p2x: number, p2y: number];
+import { IPair, ITriplet } from "./types";
 
-export interface CubicBezierCorrelator {
-	name: string;
-	cx: number;
-	bx: number;
-	ax: number;
-	cy: number;
-	by: number;
-	ay: number;
+/**
+ * @param AdjustableWeights interpolation amount, in the range [0-1], along the direction
+ */
+type InterpolationFunction<T = IPair | ITriplet> = (
+	Direction: [Base: T, Terminal: T],
+	ControlPoints: IPair<Iterable<number>>,
+	AdjustableWeights: Iterable<number>
+) => Iterable<Iterable<number>>;
 
-	/**
-	 * @param W progress [0-1]
-	 * @returns sampled X value
-	 */
-	sampleCurveX(Y: number): number;
+type EasingFunction = (TSeries: Iterable<number>) => ReturnType<InterpolationFunction>;
 
-	/**
-	 * @param X progress [0-1]
-	 * @returns sampled Y value
-	 */
-	sampleCurveY(X: number): number;
-
-	/**
-	 * @param weight progress [0-1]
-	 * @returns sampled curve derivative X value
-	 */
-	sampleCurveDerivativeX(weight: number): number;
-
-	/**
-	 * @param x progress [0-1]
-	 * @returns solved curve X value
-	 */
-	solveCurveX(x: number): number;
+export enum XCorrID {
+	Ease = 'ease',
+	Linear = 'line',
+	In = 'ease-in',
+	InCirc = 'ease-in-circ',
+	InCubic = 'ease-in-cubic',
+	InExpo = 'ease-in-expo',
+	InOut = 'ease-InOut',
+	InOutCirc = 'ease-InOut-circ',
+	InOutCubic = 'ease-InOut-cubic',
+	InOutExpo = 'ease-InOut-expo',
+	InOutQuad = 'ease-InOut-quad',
+	InOutQuart = 'ease-InOut-quart',
+	InOutQuint = 'ease-InOut-quint',
+	InOutSine = 'ease-InOut-sine',
+	InQuad = 'ease-in-quad',
+	InQuart = 'ease-in-quart',
+	InQuint = 'ease-in-quint',
+	InSine = 'ease-in-sine',
+	Out = 'ease-out',
+	OutCirc = 'ease-out-circ',
+	OutCubic = 'ease-out-cubic',
+	OutExpo = 'ease-out-expo',
+	OutQuad = 'ease-out-quad',
+	OutQuart = 'ease-out-quart',
+	OutQuint = 'ease-out-quint',
+	OutSine = 'ease-out-sine',
 }
 
-export interface CubicBezieConstructor {
-	/**
-	 * @param p1x First point horizontal position
-	 * @param p1y First point vertical position
-	 * @param p2x Second point horizontal position
-	 * @param p2y Second point vertical position
-	 * @param name An optional function name
-	 * @returns A new CubicBezier instance
-	 */
-	new(p1x: number, p1y: number, p2x: number, p2y: number, name?: string): CubicBezierCorrelator;
-}
-
-export type EasingName =
-	| 'ease'
-	| 'ease-in'
-	| 'ease-out'
-	| 'ease-in-out'
-	| 'ease-in-circ'
-	| 'ease-in-cubic'
-	| 'ease-in-expo'
-	| 'ease-in-out-circ'
-	| 'ease-in-out-cubic'
-	| 'ease-in-out-expo'
-	| 'ease-in-out-quad'
-	| 'ease-in-out-quart'
-	| 'ease-in-out-quint'
-	| 'ease-in-out-sine'
-	| 'ease-in-quad'
-	| 'ease-in-quart'
-	| 'ease-in-quint'
-	| 'ease-in-sine'
-	| 'ease-out-circ'
-	| 'ease-out-cubic'
-	| 'ease-out-expo'
-	| 'ease-out-quad'
-	| 'ease-out-quart'
-	| 'ease-out-quint'
-	| 'ease-out-sine'
-	| 'linear';
-
-export declare const BezierPointsByName: Map<EasingName, BezierPoints>;
-export declare const EaseCorrelationByName: Map<EasingName, CubicBezierCorrelator>;
-
-/** @description https://developer.mozilla.org/en-US/docs/Web/CSS/easing-function#linear */
-export declare const linear: CubicBezierCorrelator;
-/** @description https://developer.mozilla.org/en-US/docs/Web/CSS/easing-function#ease */
-export declare const ease: CubicBezierCorrelator;
-/** @description https://developer.mozilla.org/en-US/docs/Web/CSS/easing-function#ease-in */
-export declare const easeIn: CubicBezierCorrelator;
-/** @description https://developer.mozilla.org/en-US/docs/Web/CSS/easing-function#ease-out */
-export declare const easeOut: CubicBezierCorrelator;
-/** @description https://developer.mozilla.org/en-US/docs/Web/CSS/easing-function#ease-in-out */
-export declare const easeInOut: CubicBezierCorrelator;
-/** @description https://easings.net/#easeInCirc */
-export declare const easeInCirc: CubicBezierCorrelator;
-/** @description https://easings.net/#easeInCubic */
-export declare const easeInCubic: CubicBezierCorrelator;
-/** @description https://easings.net/#easeInExpo */
-export declare const easeInExpo: CubicBezierCorrelator;
-/** @description https://easings.net/#easeInOutCirc */
-export declare const easeInOutCirc: CubicBezierCorrelator;
-/** @description https://easings.net/#easeInOutCubic */
-export declare const easeInOutCubic: CubicBezierCorrelator;
-/** @description https://easings.net/#easeInOutExpo */
-export declare const easeInOutExpo: CubicBezierCorrelator;
-/** @description https://easings.net/#easeInOutQuad */
-export declare const easeInOutQuad: CubicBezierCorrelator;
-/** @description https://easings.net/#easeInOutQuart */
-export declare const easeInOutQuart: CubicBezierCorrelator;
-/** @description https://easings.net/#easeInOutQuint */
-export declare const easeInOutQuint: CubicBezierCorrelator;
-/** @description https://easings.net/#easeInOutSine */
-export declare const easeInOutSine: CubicBezierCorrelator;
-/** @description https://easings.net/#easeInQuad */
-export declare const easeInQuad: CubicBezierCorrelator;
-/** @description https://easings.net/#easeInQuart */
-export declare const easeInQuart: CubicBezierCorrelator;
-/** @description https://easings.net/#easeInQuint */
-export declare const easeInQuint: CubicBezierCorrelator;
-/** @description https://easings.net/#easeInSine */
-export declare const easeInSine: CubicBezierCorrelator;
-/** @description https://easings.net/#easeOutCirc */
-export declare const easeOutCirc: CubicBezierCorrelator;
-/** @description https://easings.net/#easeOutCubic */
-export declare const easeOutCubic: CubicBezierCorrelator;
-/** @description https://easings.net/#easeOutExpo */
-export declare const easeOutExpo: CubicBezierCorrelator;
-/** @description https://easings.net/#easeOutQuad */
-export declare const easeOutQuad: CubicBezierCorrelator;
-/** @description https://easings.net/#easeOutQuart */
-export declare const easeOutQuart: CubicBezierCorrelator;
-/** @description https://easings.net/#easeOutQuint */
-export declare const easeOutQuint: CubicBezierCorrelator;
-/** @description https://easings.net/#easeOutSine */
-export declare const easeOutSine: CubicBezierCorrelator;
-
-export default EaseCorrelationByName;
+export const performBezierInterpolation: InterpolationFunction;
+export const performHermiteInterpolation: InterpolationFunction;
+export const XCorrCTR: Map<XCorrID, IPair<IPair>>;
+export const XEaseFN: Map<XCorrID, EasingFunction>;
--- correlator.js
+++ correlator.js
@@ -1,31 +1,51 @@
-var l=Object.defineProperty;var C=(c,t)=>l(c,"name",{value:t,configurable:!0});var e=class{static{C(this,"CubicBezier")}cx;bx;ax;cy;by;ay;name;constructor(t=0,n=0,r=1,a=1,s){this.cx=3*t,this.bx=3*(r-
-t)-this.cx,this.ax=1-this.cx-this.bx,this.cy=3*n,this.by=3*(a-n)-this.cy,this.ay=1-this.cy-this.by,this.name=s||([t,n].every(
-o=>o===0)&&[r,a].every(o=>o===1)?"linear":`cubic-bezier(${[t,n,r,a].join(",")})`)}sampleCurveX(t){return((this.ax*t+this.
-bx)*t+this.cx)*t}sampleCurveY(t){return((this.ay*t+this.by)*t+this.cy)*t}sampleCurveDerivativeX(t){return(3*this.ax*t+2*
-this.bx)*t+this.cx}solveCurveX(t){if(0>=t)return 0;if(1<=t)return 1;let r=t,a=0,s=0;for(let b=0;8>b;b+=1){if(a=this.sampleCurveX(
-r)-t,Math.abs(a)<1e-6)return r;if(s=this.sampleCurveDerivativeX(r),Math.abs(s)<1e-6)break;r-=a/s}let o=0,u=1;for(r=t;o<u;){
-if(a=this.sampleCurveX(r),Math.abs(a-t)<1e-6)return r;t>a?o=r:u=r,r=o+(u-o)/2}return r}};var i=new Map([["ease",[.25,.1,.25,1]],["ease-in",[.42,0,1,1]],["ease-in-circ",[.55,0,1,.45]],["ease-in-cubic",[.32,0,.67,
-0]],["ease-in-expo",[.7,0,.84,0]],["ease-in-out-circ",[.85,0,.15,1]],["ease-in-out-cubic",[.65,0,.35,1]],["ease-in-out-e\
-xpo",[.87,0,.13,1]],["ease-in-out-quad",[.45,0,.55,1]],["ease-in-out-quart",[.76,0,.24,1]],["ease-in-out-quint",[.83,0,.17,
-1]],["ease-in-out-sine",[.37,0,.63,1]],["ease-in-out",[.42,0,.58,1]],["ease-in-quad",[.11,0,.5,0]],["ease-in-quart",[.5,
-0,.75,0]],["ease-in-quint",[.64,0,.78,0]],["ease-in-sine",[.12,0,.39,0]],["ease-out",[0,0,.58,1]],["ease-out-circ",[0,.55,
-.45,1]],["ease-out-cubic",[.33,1,.68,1]],["ease-out-expo",[.16,1,.3,1]],["ease-out-quad",[.5,1,.89,1]],["ease-out-quart",
-[.25,1,.5,1]],["ease-out-quint",[.22,1,.36,1]],["ease-out-sine",[.61,1,.88,1]],["linear",[0,0,1,1]]]),H=new e,p=new e(...i.
-get("ease"),"ease"),x=new e(...i.get("ease-in"),"ease-in"),q=new e(...i.get("ease-out"),"ease-out"),m=new e(...i.get("ea\
-se-in-out"),"ease-in-out"),B=new e(...i.get("ease-in-circ"),"ease-in-circ"),z=new e(...i.get("ease-in-cubic"),"ease-in-c\
-ubic"),g=new e(...i.get("ease-in-expo"),"ease-in-expo"),w=new e(...i.get("ease-in-out-circ"),"ease-in-out-circ"),h=new e(
-...i.get("ease-in-out-cubic"),"ease-in-out-cubic"),d=new e(...i.get("ease-in-out-expo"),"ease-in-out-expo"),y=new e(...i.
-get("ease-in-out-quad"),"ease-in-out-quad"),I=new e(...i.get("ease-in-out-quart"),"ease-in-out-quart"),O=new e(...i.get(
-"ease-in-out-quint"),"ease-in-out-quint"),v=new e(...i.get("ease-in-out-sine"),"ease-in-out-sine"),f=new e(...i.get("eas\
-e-in-quad"),"ease-in-quad"),Q=new e(...i.get("ease-in-quart"),"ease-in-quart"),E=new e(...i.get("ease-in-quint"),"ease-i\
-n-quint"),M=new e(...i.get("ease-in-sine"),"ease-in-sine"),N=new e(...i.get("ease-out-cubic"),"ease-out-cubic"),P=new e(
-...i.get("ease-out-cubic"),"ease-out-cubic"),X=new e(...i.get("ease-out-expo"),"ease-out-expo"),S=new e(...i.get("ease-o\
-ut-quad"),"ease-out-quad"),j=new e(...i.get("ease-out-quart"),"ease-out-quart"),D=new e(...i.get("ease-out-quint"),"ease\
--out-quint"),k=new e(...i.get("ease-out-sine"),"ease-out-sine"),F=new Map([["ease",p],["ease-in",x],["ease-in-circ",B],[
-"ease-in-cubic",z],["ease-in-expo",g],["ease-in-out",m],["ease-in-out-circ",w],["ease-in-out-cubic",h],["ease-in-out-exp\
-o",d],["ease-in-out-quad",y],["ease-in-out-quart",I],["ease-in-out-quint",O],["ease-in-out-sine",v],["ease-in-quad",f],[
-"ease-in-quart",Q],["ease-in-quint",E],["ease-in-sine",M],["ease-out",q],["ease-out-circ",N],["ease-out-cubic",P],["ease\
--out-expo",X],["ease-out-quad",S],["ease-out-quart",j],["ease-out-quint",D],["ease-out-sine",k]]),J=F;export{i as BezierPointsByEasingName,F as EaseCorrelationByName,J as default,p as ease,x as easeIn,B as easeInCirc,z as easeInCubic,
-g as easeInExpo,m as easeInOut,w as easeInOutCirc,h as easeInOutCubic,d as easeInOutExpo,y as easeInOutQuad,I as easeInOutQuart,
-O as easeInOutQuint,v as easeInOutSine,f as easeInQuad,Q as easeInQuart,E as easeInQuint,M as easeInSine,q as easeOut,N as easeOutCirc,
-P as easeOutCubic,X as easeOutExpo,S as easeOutQuad,j as easeOutQuart,D as easeOutQuint,k as easeOutSine,H as linear};
+var B=Object.defineProperty;var t=(r,o)=>B(r,"name",{value:o,configurable:!0});var F=t((r,o=12)=>{let a=o>0?10**o:1;return Math.sign(r)*Math.round(
+(Math.abs(r)+Number.EPSILON)*a)/a},"round");var z=t(r=>r===!0||r===!1||r==="true"||r==="on"||r==="off","isBoo\
+lean"),j=t(r=>C(r)&&Symbol.iterator in r&&typeof r[Symbol.iterator]==
+"function","isIterableObject"),h=t(r=>typeof r=="number","isNumbe\
+r"),C=t(r=>r!==null&&typeof r=="object","isObject"),A=t(r=>typeof r==
+"string","isString");function e(r,o,a,T,p){let m=t(i=>z(i)||h(i)||A(i)||j(i)||i===null,
+"isValidValue"),y=t(i=>C(i)&&Object.values(i).every(l=>m(l)),"isA\
+ditionalProperties"),I=Iterator.from((function*(){yield r,yield o,
+m(a)&&(yield a),m(T)&&(yield T)})()),f=I.toArray().length,d=!0,s={
+length:{enumerable:d,value:f}},x=Object.entries(Iterator.from(arguments).
+filter(i=>y(i)).next().value??{});for(let[i,l]of x)s[i]={enumerable:d,
+value:l};return Object.defineProperties(I,s)}t(e,"createTuple");var q=t(([r,o],[a,T],p)=>{let[m,y,I=0]=r,[f,d,s]=a,[x,i,l]=T,[O,S,
+P=1]=o;return Iterator.from(h(p)?[p]:p).map(u=>{let c=1-u,g=c**3,
+D=u**3,w=3*u*c**2,k=3*c*u**2,b=t((Q,R,n,E)=>F(Q*g+R*w+n*k+E*D),"c\
+alc");return(function*(){yield b(m,f,x,O),yield b(y,d,i,S),s!=null&&
+l!=null&&(yield b(I,s,l,P)),yield u})()})},"performBezierInterpol\
+ation"),re=t(([r,o],[a,T],p)=>{let[m,y,I=0]=r,[f,d,s]=a,[x,i,l]=T,
+[O,S,P=1]=o;return Iterator.from(h(p)?[p]:p).map(u=>{let c=u**2,g=c*
+(2*u-3)+1,D=c*(3-2*u),w=c*(u-2)+u,k=c*(u-1),b=t((Q,R,n,E)=>F(Q*g+
+R*w+n*k+E*D),"calc");return(function*(){yield b(m,f,x,O),yield b(
+y,d,i,S),s!=null&&l!=null&&(yield b(I,s,l,P)),yield u})()})},"per\
+formHermiteInterpolation"),M=(n=>(n.Ease="ease",n.Linear="line",n.
+In="ease-in",n.InCirc="ease-in-circ",n.InCubic="ease-in-cubic",n.
+InExpo="ease-in-expo",n.InOut="ease-InOut",n.InOutCirc="ease-InOu\
+t-circ",n.InOutCubic="ease-InOut-cubic",n.InOutExpo="ease-InOut-e\
+xpo",n.InOutQuad="ease-InOut-quad",n.InOutQuart="ease-InOut-quart",
+n.InOutQuint="ease-InOut-quint",n.InOutSine="ease-InOut-sine",n.InQuad=
+"ease-in-quad",n.InQuart="ease-in-quart",n.InQuint="ease-in-quint",
+n.InSine="ease-in-sine",n.Out="ease-out",n.OutCirc="ease-out-circ",
+n.OutCubic="ease-out-cubic",n.OutExpo="ease-out-expo",n.OutQuad="\
+ease-out-quad",n.OutQuart="ease-out-quart",n.OutQuint="ease-out-q\
+uint",n.OutSine="ease-out-sine",n))(M||{}),v=new Map((function*(){
+yield["ease",e(e(.25,.1),e(.25,1))],yield["ease-in",e(e(.42,0),e(
+1,1))],yield["ease-in-circ",e(e(.55,0),e(1,.45))],yield["ease-in-\
+cubic",e(e(.32,0),e(.67,0))],yield["ease-in-expo",e(e(.7,0),e(.84,
+0))],yield["ease-InOut-circ",e(e(.85,0),e(.15,1))],yield["ease-In\
+Out-cubic",e(e(.65,0),e(.35,1))],yield["ease-InOut-expo",e(e(.87,
+0),e(.13,1))],yield["ease-InOut-quad",e(e(.45,0),e(.55,1))],yield[
+"ease-InOut-quart",e(e(.76,0),e(.24,1))],yield["ease-InOut-quint",
+e(e(.83,0),e(.17,1))],yield["ease-InOut-sine",e(e(.37,0),e(.63,1))],
+yield["ease-InOut",e(e(.42,0),e(.58,1))],yield["ease-in-quad",e(e(
+.11,0),e(.5,0))],yield["ease-in-quart",e(e(.5,0),e(.75,0))],yield[
+"ease-in-quint",e(e(.64,0),e(.78,0))],yield["ease-in-sine",e(e(.12,
+0),e(.39,0))],yield["ease-out",e(e(0,0),e(.58,1))],yield["ease-ou\
+t-circ",e(e(0,.55),e(.45,1))],yield["ease-out-cubic",e(e(.33,1),e(
+.68,1))],yield["ease-out-expo",e(e(.16,1),e(.3,1))],yield["ease-o\
+ut-quad",e(e(.5,1),e(.89,1))],yield["ease-out-quart",e(e(.25,1),e(
+.5,1))],yield["ease-out-quint",e(e(.22,1),e(.36,1))],yield["ease-\
+out-sine",e(e(.61,1),e(.88,1))],yield["line",e(e(0,0),e(1,1))]})()),
+te=new Map(v.entries().map(([r,o])=>[r,a=>q([e(0,0),e(1,1)],o,a)]));export{v as XCorrCTR,M as XCorrID,te as XEaseFN,q as performBezierInterpolation,
+re as performHermiteInterpolation};
